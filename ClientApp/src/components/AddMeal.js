7928fa5f055f76c4a637501096c8e550e6c7faa4
Krzysztof Mrugała <66689589+kris007iron@users.noreply.github.com>
2023-05-25 15:48:01 +0200

    adding things to meal section

--- ClientApp/src/components/AddMeal.js
+++ ClientApp/src/components/AddMeal.js
@@ -4,81 +4,88 @@ export class AddMeal extends Component {
     static displayName = AddMeal.name;
     constructor(props) {
         super(props);
-        this.state = { foods: [], loading: true, meal: [], searchVar: ""};
+        this.state = { foods: [], loading: false, meal: [], searchVar: '' };          
     }
     componentDidMount() {
-        //this.getFoods();
+        this.getFoods();
     }
 
-    static renderFoodsTable(foods) {        
-        return (
-            <><table className="table table-striped" aria-labelledby="tableLabel">
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Add to meal</th>                            
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {foods.map(food => <tr key={food.name}>
-                            <td>{food.name}</td>
-                            <td><button onClick={() => this.AddFood(food.id)}>Add</button></td>
-                        </tr>
-                        )}
-                    </tbody>
-                </table></>
-        );
+    handleClick(id) {
+        let prevMeal = this.state.meal;
+        this.setState({ meal: [...prevMeal, this.state.foods[id]] });
     }
 
-    static renderMealTable(meal) {
+    renderFoodsTable(foods) {
         return (
-            <><table className="table table-striped" aria-labelledby="tableLabel">
+            <table className="table table-striped" aria-labelledby="tableLabel">
                 <thead>
                     <tr>
                         <th>Name</th>
-                        <th>Weight(g)</th>
+                        <th>Add to meal</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {foods.map(food => (
+                        <tr key={food.name}>
+                            <td>{food.name}</td>
+                            <td>
+                                <button onClick={() => this.handleClick(food.id)}>Add</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        );
+    }
+
+
+
+    renderMealTable(meal) {
+        return (
+            <table className="table table-striped" aria-labelledby="tableLabel">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Kcal</th>
                     </tr>
                 </thead>
                 <tbody>
                     {meal.map(food => <tr key={food.name}>
                         <td>{food.name}</td>
-                        <td><input type="number" id="quantity" name="quantity" min="1" max="3000"/></td>
+                        <td>{food.kcal}</td>
                     </tr>
                     )}
                 </tbody>
-            </table></>
+            </table>
         );
     }
+     
 
     render() {
         
-        let contents = this.state.loading
-            ? <p><em>Loading...</em></p>
-            : AddMeal.renderFoodsTable(this.state.foods);
-        let meal = <p><em>Meal is empty</em></p>
+        let contents = this.state.loading ? (
+            <p>
+                <em>Loading...</em>
+            </p>
+        ) : (
+            this.renderFoodsTable(this.state.foods)
+        );
+        let meal = <p><em>Meal is empty</em></p>;
         if (this.state.meal.length >= 1) {
-            meal = this.renderMealTable(this.meal)
+            meal = this.renderMealTable(this.state.meal);
         }
-        console.log(this)
 
         return (
-            <div>
+            <div>       
                 {meal}
                 <div>
-                    <input type="text" placeholder="Search..." onChange={event => { if (event.target.value === "") { this.getFoods() } else { this.getSpecFoods(event.target.value); this.render(); }}} />
+                    <input type="text" placeholder="Search..." onChange={event => { if (event.target.value === "") { this.getFoods() } else { this.getSpecFoods(event.target.value);}}} />
                 </div>
                 <h1 id="tableLabel">Foods</h1>
                 <p>This component demonstrates fetching data from the server.</p>
                 {contents}
             </div>
         );
-    }
-
-    async AddFood(id) {        
-        const response = await fetch('foods/getOne?id=' + id);
-        const data = await response.json();
-        this.setState({            
-            meal: this.state.meal.push(data)});
     }
 
     async getFoods() {
